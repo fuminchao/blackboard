e070e60c01657047f067acb3209432acccf2caa8
Minchao Fu <minchao.fu@blackboard.com>
2016-12-30 15:15:32 +0800

    #master x

--- script/extract-versions.sh
+++ script/extract-versions.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-BBHOME=/usr/local/blackboard
+function PrintManifest() {
+while read
+do
+  LN=`echo "$REPLY" | tr -d '\r'`
+  if [[ ${LN:0:1} = ' ' ]]; then
+    PREVLN="$PREVLN${LN:1}"
+  else
+    echo "$PREVLN"
+    PREVLN=$LN
+  fi
+done <<EOF
+`unzip -p $1 META-INF/MANIFEST.MF`
+EOF
+echo "$PREVLN"
+}
 
-find $BBHOME -name "*.jar" -type f
+find $BBHOME -name "*.[wj]ar" -type f | while read JAR
+do
+  unzip -l "$JAR" META-INF/MANIFEST.MF >> /dev/null || continue;
+
+  PrintManifest "$JAR"  | grep -E "(X\-Bb\-Repository|X\-Bb\-Commit): "
+  [[ "$?" = "0" ]] && echo "X-Bb-File: $JAR" && echo
+done
