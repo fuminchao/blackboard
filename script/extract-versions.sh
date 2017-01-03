7000262ba001e2b93ff9315c98134103026c3c56
Minchao Fu <minchao.fu@blackboard.com>
2017-01-03 15:42:33 +0800

    #master x

--- script/extract-versions.sh
+++ script/extract-versions.sh
@@ -16,7 +16,7 @@ EOF
 echo "$PREVLN"
 }
 
-for SDIR in "/usr/local/blackboard/cache/vi/BBultrainteg/plugins/astr-1025622397760/webapp/WEB-INF/lib/"
+for SDIR in "$BBHOME"
 do
   find $SDIR -name "*.[wj]ar" -type f \
     -not -path "*/backups/*" -prune \
@@ -31,12 +31,10 @@ do
   | while read JAR
   do
     unzip -l "$JAR" META-INF/MANIFEST.MF >> /dev/null || continue;
-    PrintManifest "$JAR"
-    BB_LINES=`PrintManifest "$JAR"  | grep -E "(X\-Bb\-Repository|X\-Bb\-Commit): "`
+    BB_LINES=`PrintManifest "$JAR" | sort | grep -E "(X\-Bb\-Repository|X\-Bb\-Commit): "`
     if [[ "$?" = "0" ]]; then
-      echo $BB_LINES
-      BB_REPO=`echo $BB_LINES | sort | tr '\n' ' ' | cut -d' ' -f4`
-      BB_COMM=`echo $BB_LINES | sort | tr '\n' ' ' | cut -d' ' -f2`
+      BB_REPO=`echo $BB_LINES | tr '\n' ' ' | cut -d' ' -f4`
+      BB_COMM=`echo $BB_LINES | tr '\n' ' ' | cut -d' ' -f2`
       echo "$BB_REPO#$BB_COMM $JAR"
     fi
   done
